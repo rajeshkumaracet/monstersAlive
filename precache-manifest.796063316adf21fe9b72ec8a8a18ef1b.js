3fa64cc5fe2ef7fb64255e7fafb13981e658c295
Rajeshkumar <rajeshkumaracet@gmail.com>
2019-07-05 01:11:04 +0530

    Updates

---
+++ precache-manifest.796063316adf21fe9b72ec8a8a18ef1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a00fe60d3242f78467f42450a96d0dd",
+    "url": "/monstersAlive/index.html"
+  },
+  {
+    "revision": "e45de132f51fbceb7577",
+    "url": "/monstersAlive/static/css/main.ba8646b3.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monstersAlive/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e45de132f51fbceb7577",
+    "url": "/monstersAlive/static/js/main.0e0fe966.chunk.js"
+  },
+  {
+    "revision": "cddd0fccbbbe65f023c7",
+    "url": "/monstersAlive/static/js/runtime~main.6d79ce25.js"
+  }
+]);
